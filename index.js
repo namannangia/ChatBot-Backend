7637f5de7685ec4265705602e5cb873f4eed61ea
Naman Nangia <96681933+namannangia@users.noreply.github.com>
2023-04-19 06:52:34 +0000

    apis updated

--- index.js
+++ index.js
@@ -1,68 +1,137 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { VectorDBQAChain } from "langchain/chains";
+import { index } from "./pinecone.js";
+import bodyParser from "body-parser";
+import * as redirects from "https";
+import FormData from "form-data";
+import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
+import cors from "cors";
+import fs from "fs";
 
+let data = new FormData();
+var global_text_file_name = "0.txt";
+var global_audio_file_name = "0.mp3";
+dotenv.config();
+
+function extractVideoId(url) {
+  var regExp =
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+  var match = url.match(regExp);
+  if (match && match[7].length == 11) {
+    return match[7];
+  } else {
+    return null;
+  }
+}
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-const port = process.env.PORT || 3000;
-const mongoURL = 'mongodb+srv://admin:admin@cluster0.arjtg6l.mongodb.net/?retryWrites=true&w=majority'; // replace with your own MongoDB URL
-
 const router = express.Router();
+app.use("/api", router);
+const port = process.env.PORT || 8800;
 
-// Connect to MongoDB and start the server
-MongoClient.connect(mongoURL, (err, client) => {
-  if (err) throw err;
+router.post("/fromUrlToText", (req, res) => {
+  if (extractVideoId(req.body.url) !== null) {
+    var videoId = extractVideoId(req.body.url);
+    var options = {
+      method: "GET",
+      url: "https://youtube-mp36.p.rapidapi.com/dl",
+      params: { id: videoId },
+      headers: {
+        "X-RapidAPI-Host": "youtube-mp36.p.rapidapi.com",
+        "X-RapidAPI-Key": `${process.env.RAPID_API_KEY}`,
+      },
+    };
 
-  const db = client.db('mydatabase'); // replace with your own database name
-  console.log(`Connected to MongoDB `);
+    axios
+      .request(options)
+      .then(function (response) {
+        if (response.data.status != "ok") {
+          res.status(500).json({ message: "Could not convert to mp3" });
+        } else {
+          var linkToMp3 = response.data.link;
 
-  // Define routes and start the server
-  router.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the API!' });
-  });
+          const file = fs.createWriteStream(global_audio_file_name);
+          const request = redirects.get(linkToMp3, function (response) {
+            response.pipe(file);
+            file.on("finish", () => {
+              {
+                const file2 = fs.createReadStream(global_audio_file_name);
+                if (file2) {
+                  data.append("file", file2);
+                  data.append("model", "whisper-1");
+                  let config = {
+                    method: "post",
+                    maxBodyLength: Infinity,
+                    url: "https://api.openai.com/v1/audio/transcriptions",
+                    headers: {
+                      Authorization: `Bearer ${process.env.OPEN_AI_KEY}`,
+                      ...data.getHeaders(),
+                    },
+                    data: data,
+                  };
+                  axios
+                    .request(config)
+                    .then((response) => {
+                      console.log(response.data.text);
+                      fs.writeFile(
+                        global_text_file_name,
+                        response.data.text,
+                        (err) => {
+                          if (err) throw err;
+                          const textSplitter =
+                            new RecursiveCharacterTextSplitter({
+                              chunkSize: 200,
+                              chunkOverlap: 0,
+                            });
+                          const embedder = new OpenAIEmbeddings();
 
-  router.route('/users')
-    .get((req, res) => {
-      db.collection('users').find().toArray((err, docs) => {
-        if (err) throw err;
-        res.json(docs);
+                          (async () => {
+                            const article = await fs
+                              .readFileSync(global_text_file_name)
+                              .toString();
+                            const splittedText =
+                              await textSplitter.createDocuments([article]);
+                            PineconeStore.fromDocuments(
+                              splittedText,
+                              embedder,
+                              {
+                                pineconeIndex: index,
+                                namespace: "langchain",
+                              }
+                            ).then(() =>
+                              res.status(200).send("Content recieved!")
+                            );
+                          })();
+                        }
+                      );
+                    })
+                    .catch((error) => {
+                      console.log(error);
+                    });
+                } else {
+                  res.status(200).send("File not reading");
+                }
+              }
+              return 0;
+            });
+          });
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
       });
-    })
-    .post((req, res) => {
-      db.collection('users').insertOne(req.body, (err, result) => {
-        if (err) throw err;
-        res.json({ message: 'User created!', user: result.ops[0] });
-      });
-    });
+  } else {
+    res.status(500).json({ message: "Could not extract Video id." });
+  }
+});
 
-  router.route('/users/:id')
-    .get((req, res) => {
-      const id = req.params.id;
-      db.collection('users').findOne({ _id: new ObjectId(id) }, (err, doc) => {
-        if (err) throw err;
-        res.json(doc);
-      });
-    })
-    .put((req, res) => {
-      const id = req.params.id;
-      db.collection('users').updateOne({ _id: new ObjectId(id) }, { $set: req.body }, (err, result) => {
-        if (err) throw err;
-        res.json({ message: `User with ID ${id} updated!` });
-      });
-    })
-    .delete((req, res) => {
-      const id = req.params.id;
-      db.collection('users').deleteOne({ _id: new ObjectId(id) }, (err, result) => {
-        if (err) throw err;
-        res.json({ message: `User with ID ${id} deleted!` });
-      });
-    });
-
-  app.use('/api', router);
-
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
